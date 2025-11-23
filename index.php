1462cbaed59f3a7bd4a4a0220c563ecdb5f8eee7
mohammad khorrami <mohammad.reza.khorrami123@gmail.com>
2025-11-23 22:11:35 +0330

    just for test

--- index.php
+++ index.php
@@ -1,33 +1,43 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <bold>ja hallo</bold> 
+</body>
+</html>
 
-include 'bootstrap/init.php';
+// include 'bootstrap/init.php';
 
 
-if (isset($_GET['logout'])) {
-    logout();
-}
+// if (isset($_GET['logout'])) {
+//     logout();
+// }
 
 
 
-if (!isLoggedIn()) {
+// if (!isLoggedIn()) {
 
-    //redirect to aut form
-    header("location:" . site_url('auth.php'));
-}
+//     //redirect to aut form
+//     header("location:" . site_url('auth.php'));
+// }
 
-if (isset($_GET['delet_folder']) && is_numeric($_GET['delet_folder'])) {
-    $deletedCount = deletFolder($_GET['delet_folder']);
-    // echo "$deletedCount folders succesfully deleted";
-}
+// if (isset($_GET['delet_folder']) && is_numeric($_GET['delet_folder'])) {
+//     $deletedCount = deletFolder($_GET['delet_folder']);
+//     // echo "$deletedCount folders succesfully deleted";
+// }
 
-if (isset($_GET['delet_task']) && is_numeric($_GET['delet_task'])) {
-    $deletedCount = deletTask($_GET['delet_task']);
-    // echo "$deletedCount folders succesfully deleted";
-}
-// connect to db and get tasks
-$tasks = getTask();
-$folders = getFolders();
-// dd($_SESSION);
-// var_dump($_POST);
-// var_dump($folders[0]->name);
-include 'tpl/tpl-index.php';
+// if (isset($_GET['delet_task']) && is_numeric($_GET['delet_task'])) {
+//     $deletedCount = deletTask($_GET['delet_task']);
+//     // echo "$deletedCount folders succesfully deleted";
+// }
+// // connect to db and get tasks
+// $tasks = getTask();
+// $folders = getFolders();
+// // dd($_SESSION);
+// // var_dump($_POST);
+// // var_dump($folders[0]->name);
+// include 'tpl/tpl-index.php';
